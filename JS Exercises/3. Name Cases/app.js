13b4191bcd53e4de0e84c9866433d23547efe2e4
Saad Nisar Butt <83548173+saadbutt27@users.noreply.github.com>
2022-09-30 03:00:42 +0500

    Update app.js
    added function for title case

--- JS Exercises/3. Name Cases/app.js
+++ JS Exercises/3. Name Cases/app.js
@@ -1,5 +1,21 @@
-let name = "Eric";
+let name = "eric paRkEr";
+
+let toTitleCase = (name) => {
+    let nameStr = "";
+    for (let i=0; i<name.length; i++) {
+        if (i==0){
+            nameStr += name[0].toUpperCase();
+        } else if (name[i] == " ") {
+            nameStr += " ";
+            nameStr += name[i+1].toUpperCase();
+            i++;
+        } else {
+            nameStr += name[i].toLowerCase();
+        }
+    }
+    return nameStr;
+}
 
 console.log(name.toUpperCase());
 console.log(name.toLowerCase());
-console.log(name.toUpperCase());
+console.log(toTitleCase(name));
